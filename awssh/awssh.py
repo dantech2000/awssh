9b1d1a521f7d1ca0d0f91debeadc609eb1f1667a
John Hardy <john.hardy@me.com>
2017-09-03 15:03:46 -0700

    Pause

--- awssh/awssh.py
+++ awssh/awssh.py
@@ -1,3 +1,36 @@
 # -*- coding: utf-8 -*-
 
+import boto3
+import os
+import sys
+import json
+
 """Main module."""
+
+
+class AwsHelper(object):
+
+    _clients = []
+    _default_region = 'us-west-2'
+
+    @staticmethod
+    def client(service):
+        pass
+
+    def parse_region(self, region_in=None):
+
+        if not region_in:
+            if os.environ.get("AWS_DEFAULT_REGION") is not None:
+                return os.environ.get("AWS_DEFAULT_REGION")
+            else:
+                return AwsHelper._default_region
+
+        return region_in
+
+
+class CliHelper(object):
+
+    @staticmethod
+    def ask(prompt):
+        ans = raw_input(prompt)
+        return ans
--- awssh/cli.py
+++ awssh/cli.py
@@ -3,15 +3,20 @@
 """Console script for awssh."""
 
 import click
+import awssh
 
-
-@click.command()
+@click.group()
 def main(args=None):
     """Console script for awssh."""
     click.echo("Replace this message by putting your code into "
                "awssh.cli.main")
     click.echo("See click documentation at http://click.pocoo.org/")
 
+
+@main.command()
+def testing():
+    an = awssh.CliHelper.ask("Testing")
+    click.echo('Answer: {0}'.format(an))
 
 if __name__ == "__main__":
     main()
