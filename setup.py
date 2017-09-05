0a93fd5e4a12184b195d5abdd1bf64d98885f34b
John Hardy <john.hardy@me.com>
2017-09-04 19:27:43 -0700

    Bump version: 0.8.0 → 0.9.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ test_requirements = [
 
 setup(
     name='awsshpy',
-    version='0.8.0',
+    version='0.9.0',
     description="SSH Connect to Ec2 instances",
     long_description=readme + '\n\n' + history,
     author="John Hardy",
